8d668a30312a0f7f9bb93fb52d5efb85eb2c945a
Jordan Conway <jconway@linuxfoundation.org>
2016-03-22 13:15:38 -0400

    Add travis and rspec-puppet-facts for initial tests
    Signed-off-by: Jordan Conway <jconway@linuxfoundation.org>

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,25 @@
 require 'spec_helper'
 describe 'rdiff_backup' do
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('rdiff_backup') }
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+
+      it { is_expected.to compile.with_all_deps }
+      case facts[:osfamily]
+      when 'Debian'
+        context 'with defaults for all parameters' do
+          it { should contain_class('rdiff_backup') }
+        end
+      when 'RedHat'
+        context 'with defaults for all parameters' do
+          it { should contain_class('rdiff_backup') }
+        end
+      else
+      end
+    end
   end
+
 end
