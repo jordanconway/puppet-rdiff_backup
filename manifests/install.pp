a2f2319bb155fff3cdb61a0b40e670fb323ce89a
Jordan Conway <jconway@linuxfoundation.org>
2016-03-29 15:29:42 -0400

    fixed up some parameters

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,5 +1,8 @@
 # rdiff_backup::install class
-class rdiff_backup::install {
+class rdiff_backup::install(
+  $package
+) {
+  validate_string($package)
 
   # We need some variables out of rdiff_backup::params
   include rdiff_backup::params
