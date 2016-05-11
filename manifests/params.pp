af9f4df33b12bb3839b5de374ad13b6e92f15a7b
Jordan Conway <jconway@linuxfoundation.org>
2016-05-10 22:52:51 -0400

    default remote_path

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,7 +4,7 @@ class rdiff_backup::params {
   $ensure = present
   $package = 'rdiff-backup'
   $path = undef
-  $remote_path = undef
+  $remote_path = '/srv/rdiff'
   $rdiff_server = undef
   $rdiffbackuptag = $::fqdn
 
