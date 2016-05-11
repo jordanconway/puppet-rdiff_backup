a58d651f9db2e0bb8288b586971f83016c017952
Jordan Conway <jconway@linuxfoundation.org>
2016-05-10 22:35:33 -0400

    cleaning up username

--- manifests/rdiff_export.pp
+++ manifests/rdiff_export.pp
@@ -16,15 +16,15 @@ define rdiff_backup::rdiff_export (
   create_resources('@@file', { "${::fqdn}${cleanpath} ssh rdiff user directory" => {
     ensure => directory,
     path   => "/var/lib/rdiff/${::fqdn}/${cleanpath}",
-    owner  => "${::fqdn}${cleanpath}",
-    group  => "${::fqdn}${cleanpath}",
+    owner  => "${::hostname}${cleanpath}",
+    group  => "${::hostname}${cleanpath}",
     tag    => $rdiffbackuptag,
   }})
 
   #Local resources
   # Create ssh user key for rdiff user export and collect locally
 
-  create_resources('@@user', { "${::fqdn}${cleanpath}" => {
+  create_resources('@@user', { "${::hostname}${cleanpath}" => {
     ensure  => present,
     home    => "/var/lib/rdiff/${::fqdn}/${cleanpath}",
     tag    => $rdiffbackuptag,
@@ -32,20 +32,20 @@ define rdiff_backup::rdiff_export (
 
   User <<| title == "${::fqdn}${cleanpath}" |>> { }
 
-  exec { "Create ${::fqdn}${cleanpath} user SSH key":
+  exec { "Create ${::hostname}${cleanpath} user SSH key":
     path    => '/usr/bin',
     # lint:ignore:80chars
-    command => "ssh-keygen -t rsa -N '' -C '${::fqdn}${cleanpath}@${::fqdn}' -f /var/lib/rdiff/${::fqdn}/${cleanpath}/.ssh/id_rsa",
+    command => "ssh-keygen -t rsa -N '' -C '${::hostname}${cleanpath}@${::fqdn}' -f /var/lib/rdiff/${::fqdn}/${cleanpath}/.ssh/id_rsa",
     # lint:endignore
     creates => "/var/lib/rdiff/${::fqdn}/${cleanpath}/.ssh/id_rsa",
-    user    => "${::fqdn}${cleanpath}",
+    user    => "${::hostname}${cleanpath}",
   }
 
   cron{ "${::fqdn}${cleanpath}":
     #lint:ignore:80chars
-    command => "rdiff-backup ${path} ${::fqdn}${cleanpath}@${rdiff_server}::${remote_path}/${::fqdn}/${cleanpath}",
+    command => "rdiff-backup ${path} ${::hostname}${cleanpath}@${rdiff_server}::${remote_path}/${::fqdn}/${cleanpath}",
     #lint:endignore
-    user    => "${::fqdn}${cleanpath}",
+    user    => "${::hostname}${cleanpath}",
     hour    => 1,
   }
 }
