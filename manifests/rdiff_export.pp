d0d248d5f8e031e06b2c2293ba4936bf078aadcf
Jordan Conway <jconway@linuxfoundation.org>
2016-05-17 11:58:00 -0400

    added clause for server == client

--- manifests/rdiff_export.pp
+++ manifests/rdiff_export.pp
@@ -16,20 +16,39 @@ define rdiff_backup::rdiff_export (
     $cleanpath = regsubst(regsubst($path, '\/', '_', 'G'),'_', '')
   }
 
-  concat::fragment{ "backup_${cleanpath}":
-    target  => $backup_script,
-    #lint:ignore:80chars
-    content => "rdiff-backup ${path} ${rdiff_user}@${rdiff_server}::${remote_path}/${::fqdn}/${cleanpath}\n\n",
-    #lint:endignore
-    order   => '10'
-  }
+  if ( $rdiff_server == $::fqdn){
+    concat::fragment{ "backup_${cleanpath}":
+      target  => $backup_script,
+      #lint:ignore:80chars
+      content => "rdiff-backup ${path} ${remote_path}/${::fqdn}/${cleanpath}\n\n",
+      #lint:endignore
+      order   => '10'
+    }
 
-  concat::fragment{ "retention_${cleanpath}":
-    target  => $backup_script,
-    #lint:ignore:80chars
-    content => "rdiff-backup -v0 --force --remove-older-than ${rdiff_retention} ${rdiff_user}@${rdiff_server}::${remote_path}/${::fqdn}/${cleanpath}\n\n",
-    #lint:endignore
-    order   => '15'
+    concat::fragment{ "retention_${cleanpath}":
+      target  => $backup_script,
+      #lint:ignore:80chars
+      content => "rdiff-backup -v0 --force --remove-older-than ${rdiff_retention} ${remote_path}/${::fqdn}/${cleanpath}\n\n",
+      #lint:endignore
+      order   => '15'
+    }
+  }
+  else {
+    concat::fragment{ "backup_${cleanpath}":
+      target  => $backup_script,
+      #lint:ignore:80chars
+      content => "rdiff-backup ${path} ${rdiff_user}@${rdiff_server}::${remote_path}/${::fqdn}/${cleanpath}\n\n",
+      #lint:endignore
+      order   => '10'
+    }
+
+    concat::fragment{ "retention_${cleanpath}":
+      target  => $backup_script,
+      #lint:ignore:80chars
+      content => "rdiff-backup -v0 --force --remove-older-than ${rdiff_retention} ${rdiff_user}@${rdiff_server}::${remote_path}/${::fqdn}/${cleanpath}\n\n",
+      #lint:endignore
+      order   => '15'
+    }
   }
 
   cron{ "${::fqdn}${cleanpath}":
