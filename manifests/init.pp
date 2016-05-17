e16f186e6ec5eac976dd4874a87eab4eb8e9dcce
Jordan Conway <jconway@linuxfoundation.org>
2016-05-17 09:52:51 -0400

    Cleaned up defaults

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,31 +1,24 @@
 # Class: rdiff_backup ===========================
 #
-# rdiff_backup class sets up the rdiffbackup server by default.
-# You should also configure the rdiff_backup::client class for a fully working
-# system.
+# rdiff_backup class sets up the rdiffbackup server by default.  You should
+# also configure the rdiff_backup::client class for a fully working system.
 #
 # Parameters ----------
 #
-# * `remote_path`
-# Defaults to '/srv/rdiff' The path on the rdiff-backup server that will contain
-# your backups.
+# * `remote_path` Defaults to '/srv/rdiff' The path on the rdiff-backup server
+# that will contain your backups.
 #
-# *`rdiff_user`
-# Defaults to rdiffbackup. The user that will be created on the backup server
-# and run the server side of rdiff-backup.
+# *`rdiff_user` Defaults to rdiffbackup. The user that will be created on the
+# backup server and run the server side of rdiff-backup.
 #
-# * `rdiffbackuptag`
-# Defaults to '$::fqdn'. This is used to aid resource collection on the 
-# rdiff-backup server. It you can manage different servers for different nodes 
-# with different rdiffbackuptags.
+# * `rdiffbackuptag` Defaults to '$::fqdn'. This is used to aid resource
+# collection on the rdiff-backup server. It you can manage different servers
+# for different nodes with different rdiffbackuptags.
 #
 # Examples --------
 #
-# @example class { 'rdiff_backup':
-#             remote_path    => '/srv/backups',
-#             rdiff_user     => 'backupmgr',
-#             rdiffbackuptag => 'server1backups'
-#          }
+# @example class { 'rdiff_backup': remote_path    => '/srv/backups', rdiff_user
+# => 'backupmgr', rdiffbackuptag => 'server1backups' }
 #
 # Authors -------
 #
@@ -53,14 +46,14 @@ class rdiff_backup (
   class { 'rdiff_backup::server::user':
     rdiff_user => $rdiff_user
   }
-  
+
   class {'rdiff_backup::server::import':
     rdiffbackuptag => $rdiffbackuptag
   }
 
   Anchor['rdiff_backup::begin'] ->
   Class['rdiff_backup::server::install'] ->
-  Class['rdiff_backup::server::user'] ->
+  Class['rdiff_backup::server::user'] -
   Class['rdiff_backup::server::import'] ->
   Anchor['rdiff_backup::end']
 
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,12 +1,9 @@
 # rdiff_backup::params class
 class rdiff_backup::params {
-
-  $ensure = present
   $package = 'rdiff-backup'
-  $path = undef
   $remote_path = '/srv/rdiff'
-  $rdiff_server = undef
-  $rdiffbackuptag = $::fqdn
+  $rdiff_server = "backup.${::domain}"
+  $rdiffbackuptag = 'rdiffbackuptag'
   $rdiff_user = 'rdiffbackup'
   $rdiff_retention = '1d'
   $backup_script = '/usr/local/bin/rdiff_backup.sh'
