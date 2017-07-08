46c13ca1b3d05618385f7607f6c0c3765248d733
Håkon Lerring <hakon@thedifference.be>
2017-07-08 14:58:51 +0200

    Support different group for rdiff user directory

--- manifests/server/user.pp
+++ manifests/server/user.pp
@@ -1,13 +1,14 @@
 #   class rdiff_backup::server::user
 class rdiff_backup::server::user(
   $rdiff_user,
+  $rdiff_group = "root",
 ){
   validate_string($rdiff_user)
 
   create_resources('file', {'/var/lib/rdiff' => {
     ensure => directory,
     owner  => $rdiff_user,
-    group  => 'root',
+    group  => $rdiff_group,
     mode   => '0700',
   }})
 
